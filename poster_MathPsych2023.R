63c881e63c66509f9cca4fb6883dddd1c95469f5
Luis Baroja <j.luis.baroja@gmail.com>
2023-06-26 15:37:03 -0700

    Portrait orientation

--- poster_MathPsych2023.R
+++ poster_MathPsych2023.R
@@ -2,7 +2,7 @@ rm(list=ls())
 source('hpmp.R')
 
 start_poster('poster_MathPsych2023.pdf',
-	poster_width=46.8, # A0 inches
-	poster_height=33.1, # A0 inches
-	margins=c(1,1,1,1)) # inches
+	poster_width=33.1, # A0 inches
+	poster_height=46.8, # A0 inches
+	margins=c(3,3,3,3)) # inches
 end_poster(T,T)
